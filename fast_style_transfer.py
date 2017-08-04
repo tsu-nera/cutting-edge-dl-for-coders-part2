924a7037415b23370dbc7a4e02678259caaf7b31
tsu-nera <fox10225fox@gmail.com>
2017-08-04 13:24:45 +0900

    update

--- fast_style_transfer.py
+++ fast_style_transfer.py
@@ -1,4 +1,3 @@
-import importlib
 import utils2; importlib.reload(utils2)
 from utils2 import *
 
@@ -8,13 +7,26 @@ from keras import metrics
 
 from vgg16_avg import VGG16_Avg
 
-# Tell Tensorflow to use no more GPU RAM than necessary
-limit_mem()
+arr_lr = bcolz.open('/home/ubuntu/data/trn_resized_72.bc')[:]
+arr_hr = bcolz.open('/home/ubuntu/data/trn_resized_288.bc')[:]
+
+parms = {'verbose': 0, 'callbacks': [TQDMNotebookCallback(leave_inner=True)]}
 
 def conv_block(x, filters, size, stride=(2,2), mode='same', act=True):
     x = Convolution2D(filters, size, size, subsample=stride, border_mode=mode)(x)
     x = BatchNormalization(mode=2)(x)
     return Activation('relu')(x) if act else x
+
+def res_block(ip, nf=64):
+    x = conv_block(ip, nf, 3, (1,1))
+    x = conv_block(x, nf, 3, (1,1), act=False)
+    return merge([x, ip], mode='sum')
+
+def deconv_block(x, filters, size, shape, stride=(2,2)):
+    x = Deconvolution2D(filters, size, size, subsample=stride, 
+        border_mode='same', output_shape=(None,)+shape)(x)
+    x = BatchNormalization(mode=2)(x)
+    return Activation('relu')(x)
 
 def up_block(x, filters, size):
     x = keras.layers.UpSampling2D()(x)
@@ -35,19 +47,16 @@ class ReflectionPadding2D(Layer):
         w_pad,h_pad = self.padding
         return tf.pad(x, [[0,0], [h_pad,h_pad], [w_pad,w_pad], [0,0] ], 'REFLECT')
 
+shp = arr_hr.shape[1:]
+
+style = Image.open('/home/ubuntu/data/starry_night.jpg')
+style = np.array(style)[:shp[0], :shp[1], :shp[2]]
+
 def res_crop_block(ip, nf=64):
     x = conv_block(ip, nf, 3, (1,1), 'valid')
     x = conv_block(x,  nf, 3, (1,1), 'valid', False)
     ip = Lambda(lambda x: x[:, 2:-2, 2:-2])(ip)
     return merge([x, ip], mode='sum')
-
-
-
-shp = arr_hr.shape[1:]
-style = Image.open('data/starry_night.jpg')
-style = np.array(style)[:shp[0], :shp[1], :shp[2]]
-
-shp=(288,288,3)
 
 inp=Input(shp)
 x=ReflectionPadding2D((40, 40))(inp)
@@ -60,3 +69,46 @@ x=up_block(x, 64, 3)
 x=Convolution2D(3, 9, 9, activation='tanh', border_mode='same')(x)
 outp=Lambda(lambda x: (x+1)*127.5)(x)
 
+
+vgg_inp=Input(shp)
+vgg= VGG16(include_top=False, input_tensor=Lambda(preproc)(vgg_inp))
+for l in vgg.layers: l.trainable=False
+
+def get_outp(m, ln):
+    return m.get_layer(f'block{ln}_conv2').output
+
+vgg_content = Model(vgg_inp, [get_outp(vgg, o) for o in [2,3,4,5]])
+
+style_targs = [K.variable(o) for o in
+               vgg_content.predict(np.expand_dims(style,0))]
+
+vgg1 = vgg_content(vgg_inp)
+vgg2 = vgg_content(outp)
+
+def gram_matrix_b(x):
+    x = K.permute_dimensions(x, (0, 3, 1, 2))
+    s = K.shape(x)
+    feat = K.reshape(x, (s[0], s[1], s[2]*s[3]))
+    return K.batch_dot(feat, K.permute_dimensions(feat, (0, 2, 1))
+                      ) / K.prod(K.cast(s[1:], K.floatx()))
+
+w=[0.1, 0.2, 0.6, 0.1]
+def tot_loss(x):
+    loss = 0; n = len(style_targs)
+    for i in range(n):
+        loss += mean_sqr_b(gram_matrix_b(x[i+n]) - gram_matrix_b(style_targs[i])) / 2.
+        loss += mean_sqr_b(x[i]-x[i+n]) * w[i]
+    return loss
+
+
+loss = Lambda(tot_loss)(vgg1+vgg2)
+m_style = Model([inp, vgg_inp], loss)
+targ = np.zeros((arr_hr.shape[0], 1))
+
+m_style.compile('adam', 'mae')
+
+K.set_value(m_style.optimizer.lr, 1e-4)
+m_style.fit([arr_hr, arr_hr], targ, 16, 1, **parms)
+
+top_model = Model(inp, outp)
+top_model.save_weights('/home/ubuntu/courses/deeplearning2/style_final.h5')
